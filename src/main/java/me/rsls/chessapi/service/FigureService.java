e5f54bff2e0e8a765e369cbd44c354967072be4c
Yves <yves@ruosch.me>
2020-07-19 13:30:51 +0200

    - initialize

--- src/main/java/me/rsls/chessapi/service/FigureService.java
+++ src/main/java/me/rsls/chessapi/service/FigureService.java
@@ -12,24 +12,24 @@ public class FigureService {
 
     public void createFigures(Board board) {
         HashMap<String, HashMap<Integer, Field>> tempMatrix = board.getFieldMatrix();
+        
+        tempMatrix.get("a").get(1).setFigure(new Figure(FigureType.ROOK, Color.BLACK));
+        tempMatrix.get("b").get(1).setFigure(new Figure(FigureType.KNIGHT, Color.BLACK));
+        tempMatrix.get("c").get(1).setFigure(new Figure(FigureType.BISHOP, Color.BLACK));
+        tempMatrix.get("d").get(1).setFigure(new Figure(FigureType.QUEEN, Color.BLACK));
+        tempMatrix.get("e").get(1).setFigure(new Figure(FigureType.KING, Color.BLACK));
+        tempMatrix.get("f").get(1).setFigure(new Figure(FigureType.BISHOP, Color.BLACK));
+        tempMatrix.get("g").get(1).setFigure(new Figure(FigureType.KNIGHT, Color.BLACK));
+        tempMatrix.get("h").get(1).setFigure(new Figure(FigureType.ROOK, Color.BLACK));
 
-        tempMatrix.get("a").get(8).setFigure(new Figure(FigureType.ROOK, Color.BLACK));
-        tempMatrix.get("b").get(8).setFigure(new Figure(FigureType.KNIGHT, Color.BLACK));
-        tempMatrix.get("c").get(8).setFigure(new Figure(FigureType.BISHOP, Color.BLACK));
-        tempMatrix.get("d").get(8).setFigure(new Figure(FigureType.QUEEN, Color.BLACK));
-        tempMatrix.get("e").get(8).setFigure(new Figure(FigureType.KING, Color.BLACK));
-        tempMatrix.get("f").get(8).setFigure(new Figure(FigureType.BISHOP, Color.BLACK));
-        tempMatrix.get("g").get(8).setFigure(new Figure(FigureType.KNIGHT, Color.BLACK));
-        tempMatrix.get("h").get(8).setFigure(new Figure(FigureType.ROOK, Color.BLACK));
-
-        tempMatrix.get("a").get(1).setFigure(new Figure(FigureType.ROOK, Color.WHITE));
-        tempMatrix.get("b").get(1).setFigure(new Figure(FigureType.KNIGHT, Color.WHITE));
-        tempMatrix.get("c").get(1).setFigure(new Figure(FigureType.BISHOP, Color.WHITE));
-        tempMatrix.get("d").get(1).setFigure(new Figure(FigureType.QUEEN, Color.WHITE));
-        tempMatrix.get("e").get(1).setFigure(new Figure(FigureType.KING, Color.WHITE));
-        tempMatrix.get("f").get(1).setFigure(new Figure(FigureType.BISHOP, Color.WHITE));
-        tempMatrix.get("g").get(1).setFigure(new Figure(FigureType.KNIGHT, Color.WHITE));
-        tempMatrix.get("h").get(1).setFigure(new Figure(FigureType.ROOK, Color.WHITE));
+        tempMatrix.get("a").get(8).setFigure(new Figure(FigureType.ROOK, Color.WHITE));
+        tempMatrix.get("b").get(8).setFigure(new Figure(FigureType.KNIGHT, Color.WHITE));
+        tempMatrix.get("c").get(8).setFigure(new Figure(FigureType.BISHOP, Color.WHITE));
+        tempMatrix.get("d").get(8).setFigure(new Figure(FigureType.QUEEN, Color.WHITE));
+        tempMatrix.get("e").get(8).setFigure(new Figure(FigureType.KING, Color.WHITE));
+        tempMatrix.get("f").get(8).setFigure(new Figure(FigureType.BISHOP, Color.WHITE));
+        tempMatrix.get("g").get(8).setFigure(new Figure(FigureType.KNIGHT, Color.WHITE));
+        tempMatrix.get("h").get(8).setFigure(new Figure(FigureType.ROOK, Color.WHITE));
 
         this.createPawns(tempMatrix);
         this.addCreatedFiguresToArrayList(tempMatrix, board.getFigureArrayList());
@@ -37,8 +37,8 @@ public class FigureService {
 
     public void createPawns(HashMap<String, HashMap<Integer, Field>> figuresMatrix){
         for(HashMap<Integer, Field> r : figuresMatrix.values()){
-            r.get(7).setFigure(new Figure(FigureType.PAWN, Color.BLACK));
-            r.get(2).setFigure(new Figure(FigureType.PAWN, Color.WHITE));
+            r.get(2).setFigure(new Figure(FigureType.PAWN, Color.BLACK));
+            r.get(7).setFigure(new Figure(FigureType.PAWN, Color.WHITE));
         }
     }
 

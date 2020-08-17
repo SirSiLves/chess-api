659e5d7524f9486cc23c705a61041fcc32d5b402
Yves <yves@ruosch.me>
2020-08-17 21:26:04 +0200

    started implementing rock validation

--- src/main/java/me/rsls/chessapi/model/validation/ValidateRook.java
+++ src/main/java/me/rsls/chessapi/model/validation/ValidateRook.java
@@ -1,7 +1,9 @@
 package me.rsls.chessapi.model.validation;
 
 import me.rsls.chessapi.model.Board;
+import me.rsls.chessapi.model.Color;
 import me.rsls.chessapi.model.Field;
+import me.rsls.chessapi.service.BoardService;
 
 public class ValidateRook implements IValidate {
 
@@ -31,8 +33,48 @@ public class ValidateRook implements IValidate {
     public void verifyMove() {
         int sourceNumber = sourceField.getHorizontalNumber();
         int targetNumber = targetField.getHorizontalNumber();
+        String sourceVertical = sourceField.getVertical();
+        String targetVertical = targetField.getVertical();
 
-        this.setValid(true);
+
+        if((sourceNumber == targetNumber && !sourceVertical.equals(targetVertical))
+                || (sourceNumber != targetNumber && sourceVertical.equals(targetVertical))){
+
+            if(targetField.getFigure() != null){
+                this.checkDestroy();
+            }
+            else{
+                this.checkStraightMove();
+            }
+
+//            this.setValid(true);
+        }
+
+    }
+
+
+    public void checkDestroy(){
+
+        if(!sourceField.getFigure().getFigureColor().equals(targetField.getFigure().getFigureColor())){
+            this.setValid(true);
+        }
+
+    }
+
+    public void checkStraightMove(){
+
+    }
+
+    public void checkFigureBetween(){
+
+        int sourceIndex = BoardService.VERTICAL_DESIGNATION.indexOf(sourceField.getVertical()); //7
+        int targetIndex = BoardService.VERTICAL_DESIGNATION.indexOf(targetField.getVertical()); //6 oder 8
+
+
+
+
+
+
     }
 
 
